3e3ea76b1bb8a56b2c2eee1867d44454a105f6e3
XartCircleX <bryanalexisdelgadolopez1@gmail.com>
2025-11-09 18:51:17 -0600

    Refactor GameScreen to use youtube_player_flutter for video playback and update dependencies

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class GameScreen extends StatefulWidget {
   final Map<String, String> item;
@@ -11,19 +11,25 @@ class GameScreen extends StatefulWidget {
 }
 
 class _GameScreenState extends State<GameScreen> {
-  late VideoPlayerController _controller;
+  late YoutubePlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-    final String videoUrl = widget.item['videoUrl']!;
-    _controller = videoUrl.startsWith('lib/')
-        ? VideoPlayerController.asset(videoUrl)
-        : VideoPlayerController.networkUrl(Uri.parse(videoUrl));
-    _controller.initialize().then((_) {
-      setState(() {});
-      _controller.play();
-    });
+    // URL del video de YouTube
+    const String youtubeUrl = 'https://www.youtube.com/watch?v=A6DCSSA3r0Y';
+    
+    // Extrae el ID del video de YouTube
+    final String? videoId = YoutubePlayer.convertUrlToId(youtubeUrl);
+    
+    _controller = YoutubePlayerController(
+      initialVideoId: videoId ?? '',
+      flags: const YoutubePlayerFlags(
+        autoPlay: true,
+        mute: false,
+        enableCaption: false,
+      ),
+    );
   }
 
   @override
@@ -31,12 +37,15 @@ class _GameScreenState extends State<GameScreen> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: _controller.value.isInitialized
-            ? AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              )
-            : const CircularProgressIndicator(),
+        child: YoutubePlayer(
+          controller: _controller,
+          showVideoProgressIndicator: true,
+          progressIndicatorColor: Colors.red,
+          progressColors: const ProgressBarColors(
+            playedColor: Colors.red,
+            handleColor: Colors.redAccent,
+          ),
+        ),
       ),
     );
   }
